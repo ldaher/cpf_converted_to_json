01e4ff9cd7f99185a9b2e9bfe91ac861dc7707d4
ldaher <luciano.daher@gmail.com>
2017-01-12 16:31:39 -0200

    Removed "json-converted.json" file from index; Added extra checking to remove from the result list CPF numbers larger than 11 characters (CNPJ numbers);

--- output.js
+++ output.js
@@ -16,6 +16,9 @@ var stringCounter = "0000";
 for (var i = 0; i < cpfs.length; i++) {
     var convertCounter = new String(counter);
     var replaceString = stringCounter.substring(0, stringCounter.length - convertCounter.length) + convertCounter;
+    if(cpfs[i].length > 11){
+        continue;
+    }
 
     if (i == 0) {
         jsonObject = jsonObject + "{\"item\":[";
