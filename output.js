916a47ba5af48a5858e500e4ebef8fa45de612c3
ldaher <luciano.daher@gmail.com>
2017-01-12 20:03:50 -0200

    stringbuilder module added; code improved;

--- output.js
+++ output.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var StringBuilder = require('stringbuilder');
 
 var loadFile = fs.readFileSync('cpfs.csv', 'utf8', function (err, dt) {
     if (err) {
@@ -8,32 +9,31 @@ var loadFile = fs.readFileSync('cpfs.csv', 'utf8', function (err, dt) {
     }
 });
 
-// cpfs array to hold all cpfs
-var cpfs = loadFile.split('\r\n');
 var jsonObject = "";
 var counter = 1;
 var stringCounter = "0000";
+// cpfs array to hold all cpfs
+var cpfs = loadFile.split('\r\n');
 
+jsonObject = jsonObject + '{"item":[';
 for (var i = 0; i < cpfs.length; i++) {
-    var convertCounter = new String(counter);
-    var replaceString = stringCounter.substring(0, stringCounter.length - convertCounter.length) + convertCounter;
-    
     if(cpfs[i].length > 11){
         continue;
     }
 
-    if (i == 0) {
-        jsonObject = jsonObject + "{\"item\":[";
-        jsonObject = jsonObject + "{\"cpf\": \"" + cpfs[i] + "\", \"token\": \"" + replaceString + "\", \"timestamp\": 494511390, \"uuid\": \"TESTSESSION-" + replaceString + "\"},";
-    } else if (i == cpfs.length - 1) {
-        jsonObject = jsonObject + "{\"cpf\": \"" + cpfs[i] + "\", \"token\": \"" + replaceString + "\", \"timestamp\": 494511390, \"uuid\": \"TESTSESSION-" + replaceString + "\"}";
-        jsonObject = jsonObject + "]}";
-    } else {
-        jsonObject = jsonObject + "{\"cpf\": \"" + cpfs[i] + "\", \"token\": \"" + replaceString + "\", \"timestamp\": 494511390, \"uuid\": \"TESTSESSION-" + replaceString + "\"},";
+    var numberToString = new String(counter);
+    var replacedNumber = stringCounter.substring(0, stringCounter.length - numberToString.length) + numberToString;
+
+    jsonObject = jsonObject + '{"cpf": "' + cpfs[i] + '", "token": "' + replacedNumber + '", "timestamp": 494511390, "uuid": "TESTSESSION-' + replacedNumber + '"}';
+
+    if (i != cpfs.length - 1) {
+        jsonObject = jsonObject + ', ';
     }
+    
     counter++;
 }
+jsonObject = jsonObject + ']}';
 
-console.log(jsonObject);
+console.log(jsonObject.toString());
 
 fs.writeFileSync("json-converted.json", jsonObject.toString());
